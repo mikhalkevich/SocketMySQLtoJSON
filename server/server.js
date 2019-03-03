66adf31f839e2eec43350e3b5164b52be6f57c89
mikhalkevich <mikhalkevich@ya.ru>
2019-03-03 21:05:07 +0300

    api

--- server/server.js
+++ server/server.js
@@ -6,37 +6,41 @@ var db = mysql.createConnection({
     host: 'localhost',
     user: 'root',
     database: 'shop',
-    password: ''
+    password: '2244'
 })
 
 // Log any errors connected to the db
 db.connect(function (err) {
     if (err) console.log(err)
-})
-
+});
 // Define/initialize our global vars
 var notes = []
 var isInitNotes = false
 var socketCount = 0
 global.new_id = 0;
 global.new_id_new = 0;
+global.arr = [];
 io.sockets.on('connection', function (socket) {
+    socket.emit('echo', 'server send message');
     setInterval(function () {
-        db.query('SELECT id FROM emails ORDER BY id DESC LIMIT 1', function (err, row) {
+        db.query('SELECT ID FROM device_log ORDER BY ID DESC LIMIT 1', function (err, row) {
             if (err) throw err;
-            global.new_id = row[0].id;
+            socket.emit('echo', global.arr);
+            global.new_id = row[0].ID;
             console.log(global.new_id_new, global.new_id);
             if (new_id_new != new_id) {
+                socket.emit('echo', row[0].ID + ' --- --- \n');
                 global.new_id_new = global.new_id;
-                db.query('SELECT * FROM emails ORDER BY id DESC LIMIT 100', function (err, rows) {
+                db.query('SELECT * FROM device_log ORDER BY ID DESC LIMIT 100', function (err, rows) {
                     if (err) throw err;
                     console.log('Data received from Db:\n');
                     console.log(rows);
-                    socket.emit('showrows', rows);
+                    global.arr = rows;
+                    socket.emit('echo', rows);
+                    //socket.emit('showrows', rows);
                 });
-
             }
         });
     }, 2000);
-
-})
+    console.log('connected2');
+});
